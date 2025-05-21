c5c2d362b1cf948a71b6f2b038273f756dc21de0
Decante <gustavocamposantunes@gmail.com>
2025-05-21 11:56:53 -0300

    test: ensure user is created correctly

--- src/presentation/controllers/user.controller.spec.ts
+++ src/presentation/controllers/user.controller.spec.ts
@@ -1,18 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { CreateUserUseCase } from '@/domain/usecases/user';
+import { CreateUserDto } from '../dtos';
+import { faker } from '@faker-js/faker/.';
+import { User } from '@/domain/entities';
 
 describe('UserController', () => {
   let userController: UserController;
+  let createUserUseCase: CreateUserUseCase;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        {
+          provide: 'CreateUserUseCase',
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     userController = module.get<UserController>(UserController);
+    createUserUseCase = module.get<CreateUserUseCase>('CreateUserUseCase');
   });
 
   it('should be defined', () => {
     expect(userController).toBeDefined();
+  });
+
+  it('should create a user', async () => {
+    const userData: CreateUserDto = {
+      email: faker.internet.email(),
+      name: faker.person.fullName(),
+      password: faker.internet.password(),
+    };
+
+    const expectedUser: User = {
+      id: 1,
+      name: userData.name,
+      email: userData.email,
+      password: userData.password,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    jest.spyOn(createUserUseCase, 'execute').mockResolvedValue(expectedUser);
+
+    const result = await userController.create(userData);
+
+    expect(result).toEqual(expectedUser);
+    expect(createUserUseCase.execute).toHaveBeenCalledWith(userData);
+    expect(createUserUseCase.execute).toHaveBeenCalledTimes(1);
   });
 });
