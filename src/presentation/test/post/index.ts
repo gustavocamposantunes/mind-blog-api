36f4fdaa63f0490b23dcba0c98e70219c6d7472b
Decante <gustavocamposantunes@gmail.com>
2025-05-25 14:29:51 -0300

    refactor: separate post mocks in a isolated file

---
+++ src/presentation/test/post/index.ts
@@ -0,0 +1,33 @@
+import { Post } from '@/domain/entities/post.entity';
+import { CreatePostDto } from '@/presentation/dtos/create-post.dto';
+import { faker } from '@faker-js/faker/.';
+
+export const postData: CreatePostDto = {
+  author_id: faker.number.int(),
+  title: faker.commerce.productName(),
+  content: faker.lorem.paragraphs(3),
+};
+
+export const expectedPost: Post = {
+  title: postData.title,
+  content: postData.content,
+  author_id: postData.author_id,
+  author: {
+    id: 1,
+    email: faker.internet.email(),
+    name: faker.person.fullName(),
+    password: faker.internet.password(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    posts: [],
+  },
+  id: 2,
+  publishedAt: faker.date.anytime(),
+  updatedAt: faker.date.anytime(),
+};
+
+export const mockRequest = {
+  user: {
+    userId: expectedPost.author_id,
+  },
+};
